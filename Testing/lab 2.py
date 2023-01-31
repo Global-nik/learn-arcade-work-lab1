ea31c398fdcebe2a445fc0c9847262155e617404
Global-nik <nnh2434@email.vccs.edu>
2023-01-30 23:58:22 -0500

    lab.2

--- Testing/lab 2.py
+++ Testing/lab 2.py
@@ -1,14 +1,45 @@
 import arcade
-import math
-arcade.open_window(600,600, "My Drawing")
-arcade.set_background_color((255, 255, 255))
+
+arcade.open_window(650,650, "Channel Orange")
+arcade.set_background_color((255, 115, 3))
 arcade.start_render()
-# convert to hexidecimal 66,ff,66= 66,255,66 as an example
-#arcade.draw is going to be used for project 2
-arcade.draw_circle_filled(300,300,100,arcade.csscolor.BLACK)
-arcade.draw_circle_filled(110,110,100,arcade.csscolor.BLACK)
-arcade.draw_circle_filled(490,110,100,arcade.csscolor.BLACK)
-arcade.draw_circle_filled(110,490,100,arcade.csscolor.BLACK)
-arcade.draw_circle_filled(490,490,100,arcade.csscolor.BLACK)
+#
+arcade.draw_circle_filled(325,325,100,arcade.csscolor.BLACK)
+#center
+arcade.draw_circle_filled(145,145,100,arcade.csscolor.BLACK)
+#bottom left
+arcade.draw_circle_filled(500,145,100,arcade.csscolor.BLACK)
+#bottom right
+arcade.draw_circle_filled(145,500,100,arcade.csscolor.BLACK)
+#top left
+arcade.draw_circle_filled(503,503,100,arcade.csscolor.BLACK)
+#top right
+#
+#
+arcade.draw_text('channe1', 110,400, bold=70, font_size=40)
+arcade.draw_text('O', 340,400, font_size=40)
+arcade.draw_text('R', 380,400, font_size=40)
+arcade.draw_text('A', 420,400, font_size=40)
+arcade.draw_text('N', 460,400, font_size=40)
+arcade.draw_text('G', 500,400, font_size=40)
+arcade.draw_text('E', 540,400, font_size=40)
+#
+arcade.draw_circle_outline(325,325,150,arcade.csscolor.WHITE)
+arcade.draw_rectangle_outline(325,100,200,100,arcade.csscolor.WHITE)
+arcade.draw_rectangle_outline(325,100,300,100,arcade.csscolor.BLUE)
+arcade.draw_rectangle_outline(325,100,400,100,arcade.csscolor.WHITE)
+arcade.draw_rectangle_outline(325,100,500,100,arcade.csscolor.BLUE)
+arcade.draw_rectangle_outline(325,100,600,100,arcade.csscolor.WHITE)
+arcade.draw_rectangle_outline(325,100,100,100,arcade.csscolor.BLUE)
+#
+arcade.draw_rectangle_outline(325,550,200,100,arcade.csscolor.WHITE)
+arcade.draw_rectangle_outline(325,550,300,100,arcade.csscolor.BLUE)
+arcade.draw_rectangle_outline(325,550,400,100,arcade.csscolor.WHITE)
+arcade.draw_rectangle_outline(325,550,500,100,arcade.csscolor.BLUE)
+arcade.draw_rectangle_outline(325,550,600,100,arcade.csscolor.WHITE)
+arcade.draw_rectangle_outline(325,550,100,100,arcade.csscolor.BLUE)
+#
+arcade.draw_text('Has spiraled into this', 110,250, bold=10, font_size=25)
+#arcade.draw_text('channe1', 110,400, bold=70, font_size=40)
 arcade.finish_render()
 arcade.run ()
