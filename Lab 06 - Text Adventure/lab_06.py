3e573b4799829ef9222da75406d22f20a8562ceb
Global-nik <nnh2434@email.vccs.edu>
2023-02-24 20:09:25 -0500

    feb 24th

---
+++ Lab 06 - Text Adventure/lab_06.py
@@ -0,0 +1,35 @@
+
+
+#class Room:
+   # """ """
+    #def __init__(self, description, north, south, east, west):
+       # """ """
+        #self.description = ""
+        #self.north = north
+        #self.south = south
+        #self.east = east
+        #self.west = west
+
+    #room_list = []
+
+    #teach example#
+class example:
+    def __init__(selfself, name, address, age):
+        self.name = name
+        self.address = address
+        self.age = age
+
+people = []
+for i in range(3):
+    age = input("enter age")
+    name = input("enter name")
+    address = imput ("enter address")
+    people.aapend(example(name, address, age))
+
+print(example.count)
+
+pass
+
+   # def main():
+        #my_room = class
+            #my_room.description = "dark cold room, drippy faucet"
