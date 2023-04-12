caff5757122f91766c04510306726e4efcd1bb48
Global-nik <nnh2434@email.vccs.edu>
2023-04-12 15:38:15 -0400

    lab 08 fin

--- Lab08-Sprites/lab_08.py
+++ Lab08-Sprites/lab_08.py
@@ -6,9 +6,10 @@ import math
 SPRITE_SCALING_RACOON = 0.5
 SPRITE_SCALING_BABY = .25
 SPRITE_SCALING_ADULT = 0.07
-BABY_COUNT = 50
+BABY_COUNT = 25
 ADULT_COUNT = 25
-
+ADULT_CHECK = ADULT_COUNT - 5
+BABY_CHECK = 25
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "*NEW* SUPER Racoon ball adventure"
@@ -18,10 +19,14 @@ class Baby(arcade.Sprite):
     def __init__(self, file, scale):
         super().__init__(file, scale)
 
+    def reset_pos(self):
+        self.center_y = random.randrange(SCREEN_HEIGHT + 20, SCREEN_HEIGHT + 100)
+        self.center_x = random.randrange(SCREEN_WIDTH)
+
     def update(self):
         self.center_y -= 1
-        if self.center_y == 0:
-            self.center_y = SCREEN_HEIGHT
+        if self.top < 0:
+            self.reset_pos()
 
 
 class Adult(arcade.Sprite):
@@ -48,28 +53,23 @@ class Adult(arcade.Sprite):
 class MyGame(arcade.Window):
 
     def __init__(self):
-        """ Initializer """
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
         self.adult_sound = arcade.load_sound("3dpunch2.wav")
-        #self.adult_sound = arcade.load_sound("/Users/Jxen/Desktop/learn-arcade-work/lab_08 - Sprites/punch.mp3")
         self.baby_sound = arcade.load_sound("babylaugh.wav")
-
-        # Variables that will hold sprite lists
+        #self.go_sound = arcade.load_sound("TS-fix.wav")
         self.racoon_list = None
         self.baby_list = None
         self.adult_list = None
-
-        # Set up the player info
         self.racoon_sprite = None
+        #~~~
         self.score = 0
-
-        # Don't show the mouse cursor
+        self.baby_check = 0
+        self.adult_score = 0
+        #~~~
         self.set_mouse_visible(False)
-
         self.game_over = False
         self.win = False
-
-        arcade.set_background_color(arcade.color.TEAL)
+        arcade.set_background_color(arcade.color.DIRT)
 
     def setup(self):
         """ Set up the game and initialize the variables. """
@@ -90,31 +90,19 @@ class MyGame(arcade.Window):
 
         # Create the babies
         for i in range(BABY_COUNT):
-
-            # Create the coin instance
-            # Coin image from kenney.nl
             baby = Baby("baby.png", SPRITE_SCALING_BABY)
-            #arcade.Sprite(":resources:images/items/coinGold.png", SPRITE_SCALING_COIN)
-
-            # Position the coin
             baby.center_x = random.randrange(SCREEN_WIDTH)
             baby.center_y = random.randrange(SCREEN_HEIGHT)
-
-            # Add the coin to the lists
             self.baby_list.append(baby)
 
         for i in range(ADULT_COUNT):
             adult = Adult("shotgun carl.png", SPRITE_SCALING_ADULT)
-                #arcade.Sprite(":resources:images/items/coinGold.png", SPRITE_SCALING_COIN)
-
-            # Position the coin
             """adult.circle_center_x = random.randrange(SCREEN_WIDTH)
             adult.circle_center_y = random.randrange(SCREEN_HEIGHT)
             adult.circle_radius = random.randrange(10, 200)
             adult.circle_angle = random.random() * 2 * math.pi"""
             adult.center_x = random.randrange(SCREEN_WIDTH)
             adult.center_y = random.randrange(SCREEN_HEIGHT)
-            # Add the coin to the lists
             self.adult_list.append(adult)
 
     def on_draw(self):
@@ -123,16 +111,23 @@ class MyGame(arcade.Window):
         self.baby_list.draw()
         self.adult_list.draw()
         self.racoon_list.draw()
-
-        output = f"Score: {self.score}"
+        score = f"Score: {self.score}"
+        baby_score = f"BABIES SAVED: ({self.baby_check}/25)"
+        adult_score = f"00 hits: ({self.adult_score}/5)"
+        arcade.draw_text(score, 10, 20, arcade.color.BABY_BLUE_EYES)
+        arcade.draw_text(baby_score, 10, 40, arcade.color.GLITTER)
+        arcade.draw_text(adult_score, 10, 60, arcade.color.CHROME_YELLOW)
+        #~~~
         if self.game_over:
             arcade.set_background_color(arcade.color.BLACK)
-            print("BETTER LUCK NEXT TIME", 200, 400, arcade.color.LAVA, 25)
+            arcade.draw_text("BETTER LUCK NEXT TIME", 200, 400, arcade.color.LAVA, 25)
+
+
         if self.win:
             arcade.set_background_color(arcade.color.LIGHT_SKY_BLUE)
-            print("YOU WIN", 200, 400, arcade.color.YELLOW_ROSE, 25)
-        arcade.draw_text(text=output, start_x=10, start_y=20,
-                         color=arcade.color.WHITE, font_size=14)
+            arcade.draw_text("YOU WIN", 200, 400, arcade.color.BLACK, 25)
+            arcade.draw_text(score, 10, 20, arcade.color.BLACK)
+            #arcade.play_sound(self.go_sound)
 
 
     def on_mouse_motion(self, x, y, dx, dy):
@@ -149,18 +144,20 @@ class MyGame(arcade.Window):
             self.adult_list.update()
             adult_hit_list = arcade.check_for_collision_with_list(self.racoon_sprite, self.adult_list)
             for baby in baby_hit_list:
-                baby.remove_from_sprite_lists()
+                baby.reset_pos()
                 self.score += 1
+                self.baby_check += 1
                 arcade.play_sound(self.baby_sound)
             for adult in adult_hit_list:
+
                 adult.remove_from_sprite_lists()
                 self.score -= 5
+                self.adult_score +=1
                 arcade.play_sound(self.adult_sound)
-                #arcade.play_sound(self.baby_sound)
-        #if len(self.baby_list) <= BABY_COUNT:
-            #self.win = True
-        #if len(self.adult_list) <= ADULT_COUNT:
-            #self.game_over = True
+        if len(self.baby_list) <= self.baby_check:
+            self.win = True
+        if len(self.adult_list) <= ADULT_CHECK:
+            self.game_over = True
 
 
 def main():
